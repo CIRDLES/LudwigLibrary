d1d1f0d65d2f68fd2ec3e088e07206b12a82e6b7
Jim Bowring <bowring@gmail.com>
2017-05-15 14:00:03 -0400

    updates javadocs

--- src/test/java/org/cirdles/ludwig/isoplot3/PubTest.java
+++ src/test/java/org/cirdles/ludwig/isoplot3/PubTest.java
@@ -19,21 +19,36 @@ import static org.junit.Assert.*;
  */
 public class PubTest {
 
+    /**
+     *
+     */
     public PubTest() {
     }
 
+    /**
+     *
+     */
     @BeforeClass
     public static void setUpClass() {
     }
 
+    /**
+     *
+     */
     @AfterClass
     public static void tearDownClass() {
     }
 
+    /**
+     *
+     */
     @Before
     public void setUp() {
     }
 
+    /**
+     *
+     */
     @After
     public void tearDown() {
     }
